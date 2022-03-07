fcb9fb249002bef02d15fc56b5987b9401ee3806
iamvarun <varunram35@gmail.com>
2022-03-07 19:05:16 +0530

    1st commit

--- main.py
+++ main.py
@@ -2,5 +2,12 @@ import yaml
 from yaml.loader import SafeLoader
 
 with open('verify_apache.yaml')as f:
-    data =yaml.load(f,Loader=SafeLoader)
-    print(data)
+    data =list(yaml.safe_load_all(f))
+
+#print(data[1][0]["hosts"])
+members = [{'name': 'Zoey', 'occupation': 'Doctor'},
+           {'name': 'Zaara', 'occupation': 'Dentist'}]
+with open('members.yaml','w')as f:
+    data1=yaml.dump_all(members,f,sort_keys=False,default_flow_style=False)
+
+print(data1)
